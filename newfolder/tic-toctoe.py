16af588cfafc5cfb2618c60120459fb68006a257
Meer Hassan Shah <hassanshahvlogs1@gmail.com>
2026-04-11 15:34:47 +0500

    tic

---
+++ newfolder/tic-toctoe.py
@@ -0,0 +1,86 @@
+import math
+
+board = [" " for _ in range(9)]
+
+def print_board():
+    for i in range(0, 9, 3):
+        print(board[i], "|", board[i+1], "|", board[i+2])
+
+def check_winner(player):
+    win_positions = [
+        [0,1,2],[3,4,5],[6,7,8],
+        [0,3,6],[1,4,7],[2,5,8],
+        [0,4,8],[2,4,6]
+    ]
+    for pos in win_positions:
+        if all(board[i] == player for i in pos):
+            return True
+    return False
+
+def is_draw():
+    return " " not in board
+
+def minimax(is_maximizing):
+    if check_winner("O"):
+        return 1
+    if check_winner("X"):
+        return -1
+    if is_draw():
+        return 0
+
+    if is_maximizing:
+        best = -math.inf
+        for i in range(9):
+            if board[i] == " ":
+                board[i] = "O"
+                score = minimax(False)
+                board[i] = " "
+                best = max(score, best)
+        return best
+    else:
+        best = math.inf
+        for i in range(9):
+            if board[i] == " ":
+                board[i] = "X"
+                score = minimax(True)
+                board[i] = " "
+                best = min(score, best)
+        return best
+
+def best_move():
+    best_score = -math.inf
+    move = 0
+    for i in range(9):
+        if board[i] == " ":
+            board[i] = "O"
+            score = minimax(False)
+            board[i] = " "
+            if score > best_score:
+                best_score = score
+                move = i
+    return move
+
+# Game loop
+while True:
+    print_board()
+    pos = int(input("Enter position (0-8): "))
+    board[pos] = "X"
+
+    if check_winner("X"):
+        print("You Win!")
+        break
+
+    if is_draw():
+        print("Draw!")
+        break
+
+    comp = best_move()
+    board[comp] = "O"
+
+    if check_winner("O"):
+        print("Computer Wins!")
+        break
+
+    if is_draw():
+        print("Draw!")
+        break

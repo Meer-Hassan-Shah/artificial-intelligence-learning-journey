816da3ca1d990fba7f86d5190c033e8d678a4bb6
Meer Hassan Shah <hassanshahvlogs1@gmail.com>
2026-04-18 22:39:13 +0500

    modules three  conccept

---
+++ AI USING PYTHON COURSE/week_2/module.py
@@ -0,0 +1,13 @@
+
+#calling all labraries of math
+import math
+print(math.sqrt(16))
+
+
+#calling only one function sqrt
+from math import sqrt
+print(sqrt(25))
+
+# inport the anather modules file of this modules files 
+import mymodule
+mymodule.greet()

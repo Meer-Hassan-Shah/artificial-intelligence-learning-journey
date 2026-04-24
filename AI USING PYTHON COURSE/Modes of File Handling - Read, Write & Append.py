063e9b1f2be5ffae226ccf55aabd4c08aea237fe
Meer Hassan Shah <hassanshahvlogs1@gmail.com>
2026-04-25 01:39:00 +0500

    Modes of File Handling - Read, Write & Append

--- AI USING PYTHON COURSE/Modes of File Handling - Read, Write & Append.py
+++ AI USING PYTHON COURSE/Modes of File Handling - Read, Write & Append.py
@@ -1,17 +1,16 @@
 
 
 file = open("example.txt", "r")  # Open the file in read mode
-
 print(file.read())  # This will raise an error because the file is opened in write mode
-
 file.close()  # Close the file
 
 file = open("example.txt", "w")  # Open the file in write mode
-
 file.write("change Hello, World!")  # Write a string to the file
-
 file.close()  # Close the file
 
+file = open("example.txt", "a")  # Open the file in read mode
+file.write("\nThis will raise an error because the file is opened in read mode")  # This will raise an error because the file is opened in read mode
+file.close()  # Close the file
 
 
 
